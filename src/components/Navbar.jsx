0b4282ae9fb71f253bb7ecf616968da694d47d18
Téo <teo.villet2@gmail.com>
2024-07-04 20:15:06 +0200

    First version of NavBar :
    - Need to set variables for colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../assets/logo.svg'
 import { MdMenu } from 'react-icons/md'
 
@@ -10,7 +10,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex justify-between px-5 py-5 md:px-20 bg-white select-none'>
+    <div className='flex justify-between bg-white select-none py-2 px-5 md:px-20 md:py-4'>
       <div className='flex items-center gap-x-2 cursor-pointer'>
         <img src={logo} alt='Logo' className='w-7 h-7' />
         <h2>Jean-louis Clement</h2>
@@ -26,12 +26,21 @@ const Navbar = () => {
         </button>
       </div>
       {isMenuOpen && (
-        <div className='md:hidden'>
-          <div className='hidden md:flex flex-col gap-y-4'>
-            <h3 className='cursor-pointer select-none ease-in duration-200 hover:text-cyan-600'>Accueil</h3>
-            <h3 className='cursor-pointer select-none ease-in duration-200 hover:text-cyan-600'>Le patient</h3>
-            <h3 className='cursor-pointer select-none ease-in duration-200 hover:text-cyan-600'>Analyse de la pratique</h3>
+        <div className='absolute top-0 left-0 w-full h-screen bg-white flex flex-col items-center justify-center md:hidden'>
+          <div>
+            <button onClick={toggleMenu} className='focus:outline-none bg-sky-800 p-2'>
+              <MdMenu className='w-6 h-6 text-white' />
+            </button>
           </div>
+          <h3 className='select-none' onClick={toggleMenu}>
+            Accueil
+          </h3>
+          <h3 className='select-none ' onClick={toggleMenu}>
+            Le patient
+          </h3>
+          <h3 className='select-none' onClick={toggleMenu}>
+            Analyse de la pratique
+          </h3>
         </div>
       )}
     </div>
