ef643ca20165c594cfef71b2908c03988c7195f1
teovlt <teo.villet2@gmail.com>
2024-07-07 14:15:57 +0200

    Get home file out of his folder

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import Home from '../pages/Home/Home'
+import Home from '../pages/Home'
 
 const AppRouter = () => {
   return (
