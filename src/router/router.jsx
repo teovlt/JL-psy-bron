670450c41888b46df954d532b4cc712ede0a7f21
teovlt <teo.villet2@gmail.com>
2024-07-07 14:19:31 +0200

    Update router for new pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Home from '../pages/Home'
+import Analyse from '../pages/Analyse'
+import Patient from '../pages/Patient'
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/analyse' element={<Analyse />} />
+        <Route path='/patient' element={<Patient />} />
       </Routes>
     </Router>
   )
