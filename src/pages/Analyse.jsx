0e9f988917fed6b0148c77255134e132f559f4b1
teovlt <teo.villet2@gmail.com>
2024-07-07 16:55:30 +0200

    Add colors to see the diff in the new pages

--- src/pages/Analyse.jsx
+++ src/pages/Analyse.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Analyse = () => {
-  return <div>Hello from analyse</div>
+  return <div className='bg-stone-200 h-screen w-screen'>Hello from analyse</div>
 }
 
 export default Analyse
--- src/pages/Patient.jsx
+++ src/pages/Patient.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Patient = () => {
-  return <div>Hello from patient</div>
+  return <div className='bg-stone-200 h-screen w-screen'>Hello from patient</div>
 }
 
 export default Patient
