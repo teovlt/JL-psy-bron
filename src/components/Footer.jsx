e9bc4ca28f5bb6270ed59e84cf18d1bf55141f96
Téo <teo.villet2@gmail.com>
2024-07-12 14:56:02 +0200

    Create basic footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div className='bg-primary px-10 py-20 text-white'>Footer</div>
+  return (
+    <div className='flex flex-col items-end text-white bg-primary select-none py-2 px-4 md:px-20 md:py-4'>
+      Copyright ©2024 par VILLET Téo | Tout droit reservés.
+      {/* <div className='flex items-center gap-x-2 cursor-pointer'>
+        <div className='flex items-right gap-4 text-white'>Copyright © 2024 par VILLET Téo | Tout droit reservés.</div>
+      </div> */}
+    </div>
+  )
 }
 
 export default Footer
