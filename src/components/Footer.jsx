c903e5f345dd3b54ebf7ee9816a606f82054aacb
Téo <teo.villet2@gmail.com>
2024-07-12 15:16:07 +0200

    Fix unused librarys

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex flex-col items-end text-white bg-primary select-none py-2 px-4 md:px-20 md:py-4'>
-      Copyright ©2024 par VILLET Téo | Tout droit reservés.
-      {/* <div className='flex items-center gap-x-2 cursor-pointer'>
-        <div className='flex items-right gap-4 text-white'>Copyright © 2024 par VILLET Téo | Tout droit reservés.</div>
-      </div> */}
+      Copyright ©2024 par JL-Clement | Tout droit reservés.
     </div>
   )
 }
