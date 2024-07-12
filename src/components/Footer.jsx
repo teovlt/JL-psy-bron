fc5d93cae5e7beebb4a7c33e31aa89a435bd9784
Téo <teo.villet2@gmail.com>
2024-07-12 17:07:20 +0200

    Add location to head and foot and make it responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { FaPhoneVolume } from 'react-icons/fa'
+import { FaLocationDot } from 'react-icons/fa6'
 
 const Footer = () => {
+  const call = () => {
+    window.open('tel:0682293504')
+  }
+
   return (
-    <div className='flex flex-col items-end text-white bg-primary select-none py-2 px-4 md:px-20 md:py-4'>
-      Copyright ©2024 par Jean-louis Clement | Tout droit reservés.
+    <div className='flex flex-col justify-between items-center text-white bg-primary select-none py-2 px-4 md:px-20 md:py-4'>
+      <div className='flex flex-col md:flex-row md:gap-4 items-center'>
+        <a
+          className='hover:underline cursor-pointer flex items-center gap-2'
+          target='_blank'
+          href='https://www.google.fr/maps/place/152+Av.+Franklin+Roosevelt,+69500+Bron/@45.7331485,4.906584,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4c19d4868f52f:0xfd5f8ee468e87ecb!8m2!3d45.7331448!4d4.9091589!16s%2Fg%2F11c2gpzjbm?entry=ttu'
+        >
+          <FaLocationDot />
+          <span>152 Avenue Franklin Roosevelt 69500 Bron</span>
+        </a>
+        <p className='hover:underline cursor-pointer flex items-center gap-2' onClick={call}>
+          <FaPhoneVolume />
+          <span>06 82 29 35 04</span>
+        </p>
+      </div>
+      <span className='text-center md:text-right mt-4 md:mt-0'>Copyright ©2024 par Jean-louis Clement | Tout droit reservés.</span>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
 import { FaPhoneVolume } from 'react-icons/fa'
+import { FaLocationDot } from 'react-icons/fa6'
 
 const Navbar = () => {
   const call = () => {
@@ -9,12 +10,24 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex justify-between bg-primary select-none py-2 px-5 md:px-20 md:py-4'>
-      <div className='flex items-center gap-x-2 cursor-pointer'>
-        <img src={logo} alt='Logo' className='w-7 h-7' />
-        <Link className='text-white' to={'/'}>
-          Jean-louis Clement
-        </Link>
+    <div className='flex justify-between bg-primary select-none py-2 px-5 md:px-20 md:py-2'>
+      <div className='flex items-center flex-col gap-x-2 cursor-pointer'>
+        <div className='flex items-center gap-x-2 cursor-pointer'>
+          <img src={logo} alt='Logo' className='w-7 h-7' />
+          <Link className='text-white' to={'/'}>
+            Jean-louis Clement
+          </Link>
+        </div>
+        <div className='flex items-center gap-4 text-white'>
+          <FaLocationDot />
+          <a
+            className='hidden md:inline-block text-white hover:underline'
+            target='_blank'
+            href='https://www.google.fr/maps/place/152+Av.+Franklin+Roosevelt,+69500+Bron/@45.7331485,4.906584,17z/data=!3m1!4b1!4m6!3m5!1s0x47f4c19d4868f52f:0xfd5f8ee468e87ecb!8m2!3d45.7331448!4d4.9091589!16s%2Fg%2F11c2gpzjbm?entry=ttu'
+          >
+            152 Avenue Franklin Roosevelt 69500 Bron
+          </a>
+        </div>
       </div>
       <div className='flex items-center gap-4 text-white'>
         <FaPhoneVolume />
