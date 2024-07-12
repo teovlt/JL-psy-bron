36799ad6692b0ff1fbb97499f5ceffadf1d75e62
Téo <teo.villet2@gmail.com>
2024-07-12 14:44:22 +0200

    Update NavBar and delete navLinks components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom'
 import { FaPhoneVolume } from 'react-icons/fa'
@@ -17,37 +17,11 @@ const Navbar = () => {
         </Link>
       </div>
       <div className='flex items-center gap-4 text-white'>
-        {/* <CustomNavLink label='Accueil' link='/' />
-        <CustomNavLink label='Le patient' link='/patient' />
-        <CustomNavLink label='Analyse de la pratique' link='/analyse' /> */}
         <FaPhoneVolume />
         <p className='hover:underline cursor-pointer' onClick={call}>
           06 82 29 35 04
         </p>
       </div>
-      {/* <div className='flex md:hidden items-center'>
-        <button onClick={toggleMenu} className='focus:outline-none'>
-          <MdMenu className='w-8 h-8 text-white' />
-        </button>
-      </div>
-      {isMenuOpen && (
-        <div className='fixed top-0 left-0 w-full h-full bg-white flex flex-col items-center justify-center z-50 overflow-hidden'>
-          <div className='absolute top-4 right-4'>
-            <button onClick={toggleMenu} className='focus:outline-none bg-primary p-2'>
-              <MdMenu className='w-6 h-6 text-white' />
-            </button>
-          </div>
-          <Link className='cursor-pointer select-none ease-in duration-200 py-2' to={'/'} onClick={toggleMenu}>
-            Accueil
-          </Link>
-          <Link className='cursor-pointer select-none ease-in duration-200 py-2' to={'/patient'} onClick={toggleMenu}>
-            Le patient
-          </Link>
-          <Link className='cursor-pointer select-none ease-in duration-200 py-2' to={'/analyse'} onClick={toggleMenu}>
-            Analyse de la pratique
-          </Link>
-        </div>
-      )} */}
     </div>
   )
 }
