98671fe315de6209eb7f621c99acbe735ed9c896
Téo <teo.villet2@gmail.com>
2024-07-12 15:19:20 +0200

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex flex-col items-end text-white bg-primary select-none py-2 px-4 md:px-20 md:py-4'>
-      Copyright ©2024 par JL-Clement | Tout droit reservés.
+      Copyright ©2024 par Jean-louis Clement | Tout droit reservés.
     </div>
   )
 }
